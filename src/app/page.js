039027ccb8ef75641a80b2402aeb2762de37668d
Gonzalo Perez Rojas <0xg@Gonzalos-MacBook-Pro.local>
2024-08-02 12:21:07 -0300

    agrega audio player sin funcionar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,36 @@
+"use client"
 import AboutBanner from "@/components/home/about-banner";
 import EventsBanner from "@/components/home/events-banner";
 import HomeBanner from "@/components/home/home-banner";
 import Carousel from "@/components/utils/carousel";
+import AudioPlayer from "@/components/utils/Player/AudioPlayer";
+import songsdata from "@/components/utils/Player/audios.js";
+import { useRef, useState, useEffect } from 'react';
 
 export default function Home() {
+
+  // const [songs, setSongs] = useState(songsdata);
+  // const [isplaying, setisplaying] = useState(false);
+  // const [currentSong, setCurrentSong] = useState(songsdata[0]);
+
+  // const audioElem = useRef();
+
+  // useEffect(() => {
+  //   if (isplaying) {
+  //     audioElem.current.play();
+  //   }
+  //   else {
+  //     audioElem.current.pause();
+  //   }
+  // }, [isplaying, currentSong])
+
+  // const onPlaying = () => {
+  //   const duration = audioElem.current.duration;
+  //   const ct = audioElem.current.currentTime;
+
+  //   setCurrentSong({ ...currentSong, "progress": ct / duration * 100, "length": duration })
+
+  // }
   return (
     <main className="flex min-h-screen flex-col items-center">
       <div className="min-w-screen max-w-screen overflow-hidden h-full flex flex-col items-center">
@@ -11,6 +38,9 @@ export default function Home() {
         <AboutBanner />
         <Carousel />
         <EventsBanner />
+        {/* <audio src='/audios/viajerito1.mp3' ref={audioElem} onTimeUpdate={onPlaying} />
+        <AudioPlayer songs={songs} setSongs={setSongs} isplaying={isplaying} setisplaying={setisplaying} audioElem={audioElem} currentSong={currentSong} setCurrentSong={setCurrentSong} /> */}
+
       </div>      
     </main>
   );
---
+++ src/components/utils/Player/audios.js
@@ -0,0 +1,14 @@
+exports.songsdata = [
+    {
+        "title": "0xGonzalo - viajerito1",
+        "url": "/audios/viajerito1.mp3"
+    },
+    {
+        "title": "Linking Park - In the end",
+        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    },
+    {
+        "title": "Travis Scott - Stop trina be God",
+        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    }
+]
